75f2b30c162553d007e38baa822167d4564684cf
Steve Bell <steve@colloquial.io>
2024-04-29 20:18:07 +1200

    Switch to init.lua for nvim

--- go.rb
+++ go.rb
@@ -2,7 +2,7 @@
 
 require "fileutils"
 
-META_FILES = %w(go.rb README.md gitconfig.sample)
+META_FILES = %w(go.rb README.md gitconfig.sample init.lua)
 
 Dir.chdir File.dirname(__FILE__) do
   puts "Symlinking..."
@@ -14,6 +14,9 @@ Dir.chdir File.dirname(__FILE__) do
       FileUtils.ln_s(File.expand_path(File.join(File.dirname(__FILE__), file)), "#{ENV["HOME"]}/.#{file}", :force => true)
     end
   end
+
+  puts "Symlinking init.lua to #{ENV["HOME"]}/.config/nvim/init.lua"
+  FileUtils.ln_s(File.expand_path(File.join(File.dirname(__FILE__), 'init.lua')), "#{ENV["HOME"]}/.config/nvim/init.lua")
 
   %x[curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim]
 end
---
+++ init.lua
@@ -0,0 +1,43 @@
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+Plug 'sheerun/vim-polyglot'
+Plug 'tpope/vim-rails'
+Plug 'tpope/vim-fugitive'
+Plug 'airblade/vim-gitgutter'
+Plug 'tpope/vim-surround'
+Plug 'tpope/vim-unimpaired'
+Plug 'tpope/vim-rhubarb'
+Plug 'dsawardekar/portkey'
+Plug 'elzr/vim-json'
+Plug 'noprompt/vim-yardoc'
+Plug 'othree/html5.vim'
+Plug 'pangloss/vim-javascript'
+Plug 'ap/vim-css-color'
+Plug 'ynkdir/vim-vimlparser'
+Plug 'syngan/vim-vimlint'
+Plug 'hashivim/vim-hashicorp-tools'
+Plug 'apeschel/vim-syntax-syslog-ng'
+Plug('prettier/vim-prettier', {['do'] = vim.fn['yarn install']})
+Plug('junegunn/fzf', {['do'] = vim.fn['fzf#install'] })
+Plug 'junegunn/fzf.vim'
+Plug 'vim-test/vim-test'
+Plug 'godlygeek/tabular'
+Plug 'tomtom/tcomment_vim'
+Plug 'henrik/rename.vim'
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+Plug 'dyng/auto_mkdir'
+Plug 'chriskempson/base16-vim'
+Plug 'jxnblk/vim-mdx-js'
+Plug('fatih/vim-go', {['do'] = vim.fn[':GoUpdateBinaries'] })
+Plug 'williamboman/mason.nvim'
+Plug 'williamboman/mason-lspconfig.nvim'
+Plug 'neovim/nvim-lspconfig'
+
+vim.call('plug#end')
+
+vim.cmd('source ~/.vim/core.vim')
+vim.cmd('source ~/.vim/plugins.vim')
+vim.cmd('source ~/.vim/mappings.vim')
